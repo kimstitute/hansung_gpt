a3e9d0d4c549679d89b9eb3c9dae60f7b03fee72
kimstitute <152252175+kimstitute@users.noreply.github.com>
2024-11-12 11:54:53 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import streamlit as st
 # from dotenv import load_dotenv
 # load_dotenv()
 
-from langchain_openai import ChatOpenAI
+from langchain.chat_models import ChatOpenAI
+#from langchain_openai import ChatOpenAI
 # from langchain.chat_models import ChatOpenAI
 chat_model = ChatOpenAI()
 
